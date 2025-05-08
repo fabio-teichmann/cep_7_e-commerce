5457ef8a7eb8c0f538fd6651af435911f715bf45
Fabio Teichmann <fabio.teichmann15@gmail.com>
2025-05-08 12:25:27 +0300

    corrected tf dev script

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -10,10 +10,13 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-      region = var.aws_region
     }
   }
   required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
 }
 
 data "aws_availability_zones" "available" {}
