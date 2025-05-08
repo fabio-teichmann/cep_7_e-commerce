78dce448f1f2ac90a9f2ddd5962e95bb00e4959d
Fabio Teichmann <fabio.teichmann15@gmail.com>
2025-05-08 21:37:59 +0300

    Trigger GHA reindex

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -73,6 +73,14 @@ module "eks" {
   }
 }
 
+resource "aws_security_group_rule" "allow_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.eks.node_security_group_id
+}
 
 
 # provider "helm" {
